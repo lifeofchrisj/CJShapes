4aadd642a93161be2cc917236a19375e9a604be1
Christopher Johnson <cjamisonj5@hotmail.com>
2016-02-15 17:53:45 -0700

    Shape Tester
    Shape Tester

---
+++ ShapeTester.py
@@ -0,0 +1,15 @@
+from Box import Box
+from Pyramid import Pyramid
+from Sphere import Sphere
+
+buildBox = Box(input,input,input,input)
+
+Box()
+
+buildPyramid = Pyramid(input,input,input)
+
+Pyramid()
+
+buildSphere = Sphere(input)
+
+Sphere()
